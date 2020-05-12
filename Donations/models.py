438c9c48394af66175f43d9a96431fac699b4297
Ruizhi Zhang <Rachel@Ruizhis-MBP.T-mobile.com>
2020-05-11 18:59:36 -0700

    change instruction and survey text

--- Donations/models.py
+++ Donations/models.py
@@ -112,6 +112,9 @@ class Player(BasePlayer):
             [0, 'The rebate makes my donation appear less generous'],
             [1, 'The rebate has an advertising effect that will attract more friends to donate']],
         widget=widgets.RadioSelect)
+    s15a = models.IntegerField(widget=widgets.NumberInput(attrs={'autocomplete': 'off'}))
+    s15b = models.IntegerField(widget=widgets.NumberInput(attrs={'autocomplete': 'off'}))
+    s15c = models.IntegerField(widget=widgets.NumberInput(attrs={'autocomplete': 'off'}))
 
     # pr is the paying round: this is randomly chosen 
     # in the Results class in Pages.py
